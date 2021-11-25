38037123fcb68725a64028da3749c3b7f4a40a9d
Diogo-Ol <oliveiradiogo50@gmail.com>
2021-11-25 14:36:17 -0300

    Implementando seção de administração na home

---
+++ Componentes/index.js
@@ -0,0 +1,8 @@
+import { Carousel } from "./carrousel.js";
+
+const anterior = '[data-anterior]'
+const proximo = '[data-proximo]'
+const listaProdutos = '[data-lista-perfil]'
+
+
+new Carousel(anterior, proximo, listaProdutos)
