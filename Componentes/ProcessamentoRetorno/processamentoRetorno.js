927dfa85ee3f6cb025320f8c52250cb4736c37cc
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-02-15 09:52:21 -0300

    Finalizando javascript do filtro em Processamento de Retorno

---
+++ Componentes/ProcessamentoRetorno/processamentoRetorno.js
@@ -0,0 +1,6 @@
+function abrirResultadoretornoFiltro (){
+    resultadoRetornoFiltro.style.opacity = ` 1 `
+    resultadoRetornoFiltro.style.display = ` flex `
+}
+
+const resultadoRetornoFiltro = document.querySelector('#resultado__retorno___pesquisa-filtro')
