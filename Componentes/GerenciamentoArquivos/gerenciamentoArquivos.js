875b8ac9ffaf2063481b9320c7a3c0a61232c921
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-02-15 15:29:48 -0300

    Finalizando tela de gerenciamento de arquivos

---
+++ Componentes/GerenciamentoArquivos/gerenciamentoArquivos.js
@@ -0,0 +1,6 @@
+function abrirResultadoDownload (){
+    resultadoDownload.style.opacity = ` 1 `
+    resultadoDownload.style.display = ` flex `
+}
+
+const resultadoDownload = document.querySelector('#resultado__download___pesquisa-filtro')
